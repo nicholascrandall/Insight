477849e6c33731da724a1395005ddf755b0caf71
Nicholas <nickycrandall@gmail.com>
2021-05-26 15:47:29 -0400

    made initial stock page

---
+++ src/components/Stock.js
@@ -0,0 +1,18 @@
+import { Component } from 'react'
+
+export default class Stock extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return(
+            <p>stock test</p>
+        )
+    }
+    
+}
